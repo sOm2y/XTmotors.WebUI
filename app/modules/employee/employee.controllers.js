c7720738b5f952981a9cdff1f2319182493f991f
sOm2y <yyin888@aucklanduni.ac.nz>
2015-10-23 19:44:27 +1300

    employee api setup

--- app/modules/employee/employee.controllers.js
+++ app/modules/employee/employee.controllers.js
@@ -8,6 +8,12 @@
  * Main controller of the application.
  */
 angular.module('employee.controllers',[])
-	.controller('EmployeeCtrl', ['$scope', function ($scope) {
-		$scope.employee = 'employee'; 
+	.controller('EmployeeCtrl', 'xtmotorsAPIService', '$q',['$scope', function ($scope, xtmotorsAPIService, $q) {
+		// $scope.employee = 'employee'; 
+		xtmotorsAPIService.get({section:'employee'})
+		.$promise.then(function(employees){
+			$scope.employees = employees;
+		},function(error){
+			console.log(error);
+		});
 	}]);
