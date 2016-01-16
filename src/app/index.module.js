e94a04fc95e23e2526f02d52c2838380be05b231
sOm2y <yyin888@aucklanduni.ac.nz>
2016-01-16 14:56:57 +1300

    init

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('xtmotorsWebUi', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
