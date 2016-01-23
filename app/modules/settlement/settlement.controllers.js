4565ea4e00b753db31847e19a894667f409e8965
sOm2y <yyin888@aucklanduni.ac.nz>
2016-01-23 19:20:05 +1300

    settlement setup

--- app/modules/settlement/settlement.controllers.js
+++ app/modules/settlement/settlement.controllers.js
@@ -12,6 +12,4 @@ angular.module('settlement.controllers',[])
 		$scope.settlement = 'settlement'; 
 		$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
         $scope.data = [300, 500, 100, 40, 120];
-
-       
 	}]);
