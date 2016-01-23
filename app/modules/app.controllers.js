f2b82672a057d0777be43d0a4cbc7c29bac43db7
sOm2y <yyin888@aucklanduni.ac.nz>
2016-01-23 19:19:23 +1300

    main js setup and inject sample data

--- app/modules/app.controllers.js
+++ app/modules/app.controllers.js
@@ -17,10 +17,9 @@ angular.module('app.controllers',[])
          $rootScope.buttonDisable = false;
         if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
           event.preventDefault();
-
           loginModal()
             .then(function () {
-              return $state.go(toState.name, toParams);
+              directCarDetails();
             })
             .catch(function () {
               return $state.go('car');
@@ -33,6 +32,31 @@ angular.module('app.controllers',[])
       }, function(error) {
         console.log(error);
       });
+      $rootScope.cars=[
+        {CarId:'999-999',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'},
+        {CarId:'000-116',Brand:'Mazda',Model:'M3',Year:'2003',Odometer:'23000',SalePrice:'73000',Status:'arrived'}
+      ];
 
     $rootScope.changeView = function(view) {
       $location.path(view);
@@ -45,8 +69,16 @@ angular.module('app.controllers',[])
       delete $rootScope.currentUser;
       loginModal().then(function () {
         // $rootScope.removeAlerts();
-        return $state.go($state.current, {}, {reload: true});
+        directCarDetails();
       });   
+    };
+
+    function directCarDetails(){
+      if($state.current.name === 'car.details'){
+        return $state.go('car');
+      }else{
+        return $state.go($state.current, {}, {reload: true});
+      }
     };
 
 
@@ -82,99 +114,103 @@ angular.module('app.controllers',[])
 		$scope.listGalleryView = false;
 
 
-    $scope.toggleSidenav = function(menuId) {
-        $mdSidenav(menuId).toggle();
-      };
-      $scope.menu = [
-        {
-          link : 'car',
-          title: 'Cars',
-          icon: 'car'
-        },
-        {
-          link : 'settlement',
-          title: 'Settlement',
-          icon: 'group'
-        },
-        {
-          link : 'storage',
-          title: 'Storage',
-          icon: 'message'
-        },
-        {
-          link : 'consignment',
-          title: 'Consignments',
-          icon: 'message'
-        }
-      ];
-      $scope.admin = [
-        {
-          link : 'customer',
-          title: 'Customer',
-          icon: 'delete'
-        },
-        {
-          link : 'employee',
-          title: 'Employee',
-          icon: 'settings'
-        }
-      ];
-      $scope.activity = [
-          {
-            what: 'Brunch this weekend?',
-            who: 'Ali Conners',
-            when: '3:08PM',
-            notes: " I'll be in your neighborhood doing errands"
-          },
-          {
-            what: 'Summer BBQ',
-            who: 'to Alex, Scott, Jennifer',
-            when: '3:08PM',
-            notes: "Wish I could come out but I'm out of town this weekend"
-          },
-          {
-            what: 'Oui Oui',
-            who: 'Sandra Adams',
-            when: '3:08PM',
-            notes: "Do you have Paris recommendations? Have you ever been?"
-          },
-          {
-            what: 'Birthday Gift',
-            who: 'Trevor Hansen',
-            when: '3:08PM',
-            notes: "Have any ideas of what we should get Heidi for her birthday?"
-          },
-          {
-            what: 'Recipe to try',
-            who: 'Brian Holt',
-            when: '3:08PM',
-            notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
-          },
-        ];
+    
+  // Toolbar search toggle
+  $scope.toggleSearch = function(element) {
+    $scope.showSearch = !$scope.showSearch;
+  };
+  
+  // Sidenav toggle
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+  
+  // Menu items
+  $scope.menu = [
+    {
+      link : 'car',
+      title: 'Cars',
+      icon: 'action:ic_dashboard_24px' // we have to use Google's naming convention for the IDs of the SVGs in the spritesheet
+    },
+    {
+      link : 'settlement',
+      title: 'Settlement',
+      icon: 'social:ic_group_24px'
+    },
+    {
+      link : 'storage',
+      title: 'Storage',
+      icon: 'communication:ic_message_24px'
+    },
+    {
+      link : 'consignment',
+      title: 'Consignments',
+      icon: 'communication:ic_message_24px'
+    }
+  ];
+  $scope.admin = [
+    {
+      link : 'customer',
+      title: 'Customer',
+      icon: 'action:ic_delete_24px'
+    },
+    {
+      link : 'employee',
+      title: 'Employee',
+      icon: 'action:ic_settings_24px'
+    }
+  ];
+  
+  // Mock activity
+  $scope.activity = [
+      {
+        what: 'Brunch this weekend?',
+        who: 'Ali Conners',
+        avatar: 'svg-1',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        what: 'Summer BBQ',
+        who: 'to Alex, Scott, Jennifer',
+        avatar: 'svg-2',
+        when: '3:08PM',
+        notes: "Wish I could come out but I'm out of town this weekend"
+      },
+      {
+        what: 'Oui Oui',
+        who: 'Sandra Adams',
+        avatar: 'svg-3',
+        when: '3:08PM',
+        notes: "Do you have Paris recommendations? Have you ever been?"
+      },
+      {
+        what: 'Birthday Gift',
+        who: 'Trevor Hansen',
+        avatar: 'svg-4',
+        when: '3:08PM',
+        notes: "Have any ideas of what we should get Heidi for her birthday?"
+      },
+      {
+        what: 'Recipe to try',
+        who: 'Brian Holt',
+        avatar: 'svg-5',
+        when: '3:08PM',
+        notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
+      },
+    ];      
+    // Bottomsheet & Modal Dialogs
+    $scope.alert = '';
+    $scope.showListBottomSheet = function($event) {
       $scope.alert = '';
-      $scope.showListBottomSheet = function($event) {
-        $scope.alert = '';
-        $mdBottomSheet.show({
-          template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
-          controller: 'ListBottomSheetCtrl',
-          targetEvent: $event
-        }).then(function(clickedItem) {
-          $scope.alert = clickedItem.name + ' clicked!';
-        });
-      };
-      
-      // $scope.showAdd = function(ev) {
-      //   $mdDialog.show({
-      //     controller: DialogController,
-      //     template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
-      //     targetEvent: ev,
-      //   })
-      //   .then(function(answer) {
-      //     $scope.alert = 'You said the information was "' + answer + '".';
-      //   }, function() {
-      //     $scope.alert = 'You cancelled the dialog.';
-      //   });
-      // };
+      $mdBottomSheet.show({
+        template: '<md-bottom-sheet class="md-list md-has-header"><md-list><md-list-item class="md-2-line" ng-repeat="item in items" role="link" md-ink-ripple><md-icon md-svg-icon="{{item.icon}}" aria-label="{{item.name}}"></md-icon><div class="md-list-item-text"><h3>{{item.name}}</h3></div></md-list-item> </md-list></md-bottom-sheet>',
+        controller: 'ListBottomSheetCtrl',
+        targetEvent: $event
+      }).then(function(clickedItem) {
+        $scope.alert = clickedItem.name + ' clicked!';
+      });
+    };
 
 	}])
 .controller('LoginModalCtrl', function ($scope) {
@@ -187,17 +223,60 @@ angular.module('app.controllers',[])
 })
 .controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
   $scope.items = [
-    { name: 'Share', icon: 'share' },
-    { name: 'Upload', icon: 'upload' },
-    { name: 'Copy', icon: 'copy' },
-    { name: 'Print this page', icon: 'print' },
+    { name: 'Share', icon: 'social:ic_share_24px' },
+    { name: 'Upload', icon: 'file:ic_cloud_upload_24px' },
+    { name: 'Copy', icon: 'content:ic_content_copy_24px' },
+    { name: 'Print this page', icon: 'action:ic_print_24px' },
   ];
   
   $scope.listItemClick = function($index) {
     var clickedItem = $scope.items[$index];
     $mdBottomSheet.hide(clickedItem);
   };
-});
+})
+.controller('DemoCtrl', DemoCtrl);
+  function DemoCtrl ($timeout, $q) {
+    var self = this;
+    // list of `state` value/display objects
+    self.states        = loadAll();
+    self.selectedItem  = null;
+    self.searchText    = null;
+    self.querySearch   = querySearch;
+    // ******************************
+    // Internal methods
+    // ******************************
+    /**
+     * Search for states... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.states.filter( createFilterFor(query) ) : [];
+      return results;
+    }
+    /**
+     * Build `states` list of key/value pairs
+     */
+    function loadAll() {
+      var allStates = 'Ali Conners, Alex, Scott, Jennifer, \
+              Sandra Adams, Brian Holt, \
+              Trevor Hansen';
+      return allStates.split(/, +/g).map( function (state) {
+        return {
+          value: state.toLowerCase(),
+          display: state
+        };
+      });
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      };
+    }
+  };
 
 function DialogController($scope, $mdDialog) {
   $scope.hide = function() {
