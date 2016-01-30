be4e340436a6faad555f6b7f221507f74feb4be8
sOm2y <yyin888@aucklanduni.ac.nz>
2016-01-30 16:15:24 +1300

    employee pagination setup

--- app/modules/employee/employee.controllers.js
+++ app/modules/employee/employee.controllers.js
@@ -8,14 +8,33 @@
  * employee controller of the application.
  */
 angular.module('employee.controllers',[])
-	.controller('EmployeeCtrl', [ '$scope','xtmotorsAPIService','$q', function ($scope, xtmotorsAPIService, $q ) {
+	.controller('EmployeeCtrl', ['$rootScope','$scope','xtmotorsAPIService','$q', function ($rootScope,$scope, xtmotorsAPIService, $q ) {
 		// $scope.employee = 'employee'; 
+		$rootScope.isLoading = true;
 		xtmotorsAPIService.query({section:'Employee'})
 		.$promise.then(function(employees){
 			$scope.employees = employees;
+			$scope.totalEmployees = $scope.employees.length;
+		    $scope.totalPages = 10;
+		   	$scope.pagination = {
+		        currentPage:  1
+		    };        
+		    $scope.employeesPerPage = 20;
+			$scope.paginatedEmployees = $scope.employees.slice(0, $scope.employeesPerPage);
+
+		    $scope.pageChanged = function(){
+		         // $scope.currentPage = 1;
+		        var begin = (($scope.pagination.currentPage - 1) * $scope.employeesPerPage),
+		            end   = begin + $scope.employeesPerPage;
+
+		        $scope.paginatedEmployees = $scope.employees.slice(begin, end);
+
+		    };
 		},function(error){
 			console.log(error);
-		});
+		}).finally(function(){
+	       $rootScope.isLoading = false;
+	    });
 	}])
 	.controller('EmployeeDetailsCtrl', ['$scope', function ($scope) {
 		
