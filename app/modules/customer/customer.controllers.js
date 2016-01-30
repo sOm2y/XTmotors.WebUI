0ca6af1ca98e110c575b50af52dfc37a9a05ad2f
sOm2y <yyin888@aucklanduni.ac.nz>
2016-01-30 16:15:41 +1300

    customer pagination setup

--- app/modules/customer/customer.controllers.js
+++ app/modules/customer/customer.controllers.js
@@ -8,14 +8,38 @@
  * customer controller of the application.
  */
 angular.module('customer.controllers',[])
-	.controller('CustomerCtrl', ['$scope','xtmotorsAPIService','$q', function ($scope,xtmotorsAPIService,$q) {
-		$scope.customer = 'customer'; 
-		xtmotorsAPIService.query({section:'Customer'})
-		.$promise.then(function(customer){
-			$scope.customers = customer;
-		},function(error){
-			console.log(error);
-		});
+	.controller('CustomerCtrl', ['$rootScope','$scope','xtmotorsAPIService','$q', function ($rootScope,$scope,xtmotorsAPIService,$q) {
+		if(!$scope.customers){
+			xtmotorsAPIService.query({section:'Customer'})
+			.$promise.then(function(customer){
+				$scope.customers = customer;
+
+				$scope.totalCustomers = $scope.customers.length;
+		        $scope.totalPages     = 10;
+		        $scope.pagination = {
+		            currentPage:  1
+		        };        
+		        $scope.customersPerPage    = 20;
+		        
+		        
+		        $scope.paginatedCustomers = $scope.customers.slice(0, $scope.customersPerPage);
+
+		        $scope.pageChanged = function(){
+		         // $scope.currentPage = 1;
+		          var begin = (($scope.pagination.currentPage - 1) * $scope.customersPerPage),
+		              end   = begin + $scope.customersPerPage;
+
+		          $scope.paginatedCustomers = $scope.customers.slice(begin, end);
+
+		        };
+			},function(error){
+				console.log(error);
+			})
+			.finally(function(){
+		       $rootScope.isLoading = false;
+		    });
+		}
+		
 	}])
 	.controller('CustomerDetailsCtrl', ['$scope', function ($scope) {
 		
