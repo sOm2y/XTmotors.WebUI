a8bbb1adff212d70ca4b4bdb4ce59d642e8f98b9
sOm2y <yyin888@aucklanduni.ac.nz>
2016-05-24 10:06:24 +1200

    sketch basic ui for consignment page create basic ui for consignment details pge

--- app/modules/consignment/consignment.controllers.js
+++ app/modules/consignment/consignment.controllers.js
@@ -8,15 +8,47 @@
  * consignment controller of the application.
  */
 angular.module('consignment.controllers',[])
-	.controller('ConsignmentCtrl', ['$scope','xtmotorsAPIService', function ($scope,xtmotorsAPIService) {
+	.controller('ConsignmentCtrl', ['$scope','xtmotorsAPIService','$state',function ($scope,xtmotorsAPIService,$state) {
 		$scope.consignment = 'consignment'; 
 		$scope.countToPaid = 20408;
 		$scope.countFromPaid = 0;
 		$scope.countToUnpaid = 10403;
 		$scope.countFromUnpaid = 0;
 
+		$scope.options = {
+	      autoSelect: true,
+	      boundaryLinks: false,
+	      largeEditDialog: false,
+	      pageSelector: false,
+	      rowSelection: false
+	    };
+	        
+	    $scope.query = {
+	      order: 'batchId',
+	      limit: 15,
+	      page: 1
+	    };
+
+    
 		xtmotorsAPIService.query({ section:'Imports/'})
 		.$promise.then(function(imports){
 			$scope.imports = imports;
+			$scope.editImport = function(importDetail){
+				$scope.import = importDetail;
+				$state.go('consignment.details',{batchId:$scope.import.batchId});
+				// $scope.customer.dob = changeDateFormat(customer.dob);
+
+			};
+			$scope.backToConsignment = function(){
+				$state.go('consignment');
+			};
+
 		});
+	}])
+	.controller('ConsignmentDetailsCtrl', ['$scope','xtmotorsAPIService' ,function ($scope,xtmotorsAPIService) {
+		xtmotorsAPIService.query({section:'ImportRecords/'})
+		.$promise.then(function(importRecords){
+			$scope.importRecords = importRecords;
+		});
+		
 	}]);
