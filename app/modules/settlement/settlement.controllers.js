6aa0b23b646136fb6fc8ef6a204be74d59280c49
Lucas Zhang <zhangysd@hotmail.com>
2016-06-24 22:47:26 +1200

    Finish settlement page.

--- app/modules/settlement/settlement.controllers.js
+++ app/modules/settlement/settlement.controllers.js
@@ -8,43 +8,144 @@
  * settlement controller of the application.
  */
 angular.module('settlement.controllers',[])
-	.controller('SettlementCtrl', ['$scope','xtmotorsAPIService', function ($scope,xtmotorsAPIService) {
+	.controller('SettlementCtrl', ['$rootScope', '$scope','xtmotorsAPIService', function ($rootScope,$scope,xtmotorsAPIService) {
 		$scope.settlement = 'settlement';
-		$scope.dates = {
-				'January':[],
-				'February':[],
-				'March':[],
-				'April':[],
-				'May':[],
-				'June':[],
-				'July':[],
-				'August':[],
-				'Sepetember':[],
-				'October':[],
-				'November':[],
-				'December':[]
-		};
-		xtmotorsAPIService.query({section:'contracts'}).$promise.then(function(contracts){
-			$scope.contracts = contracts;
-			_.forEach(contracts,function(contract){
-				var date = moment(contract.contractDate).format('MMMM');
-				switch (date) {
-					case 'June':
-						$scope.dates.June.push(contract);
-						return;
-					default:
-						return;
-				}
-			})
-		},function(error){
-
-		});
-
-		$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+		$scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "Sepetember", "October", "November", "December"];
 		$scope.series = ['Series A'];
-		$scope.data = [
-		    [65, 59, 80, 81, 56, 55, 40]
-		];
+		
+		var contractTemp = [];
+		var importTemp = [];
+		$scope.contractData = [];
+		$scope.importData = [];
+
+		function Dates(){
+			this.January = [];
+			this.February = [];
+			this.March = [];
+			this.April = [];
+			this.May = [];
+			this.June = [];
+			this.July = [];
+			this.August = [];
+			this.Sepetember = [];
+			this.October = [];
+			this.November = [];
+			this.December = [];
+		}
+
+		$scope.contractDates = new Dates();
+		$scope.importDates = new Dates();
+
+		drawGraphs();
+
+		function drawGraphs(){
+			getContracts();
+			getImports();
+		}
+
+		var count = 0;
+		function getImports(){
+			xtmotorsAPIService.query({section:'car/summary'})
+			.$promise.then(function(cars) {
+				_.forEach(cars, function(item){
+					xtmotorsAPIService.get({section:'ImportRecords/' + item.carId})
+					.$promise.then(function(car) {
+						//console.log(car);
+						xtmotorsAPIService.get({section:'Imports/' + car.batchId})
+						.$promise.then(function(batch) {
+							var date = getMonthName(batch, 'createTime');
+							//console.log(date);
+							addToDates($scope.importDates, date, batch);
+							count++;
+							if(count === cars.length){
+								getCountNumber(importTemp, $scope.importDates, $scope.importData);
+							}
+						});
+					});
+				})
+				
+			    
+
+			});
+		}
+
+		function getContracts(){
+			xtmotorsAPIService.query({section:'contracts'})
+			.$promise.then(function(contracts){
+				$scope.contracts = contracts;
+				_.forEach(contracts, function(contract){
+					var date = getMonthName(contract, 'contractDate');
+					addToDates($scope.contractDates, date, contract);
+				})
+				getCountNumber(contractTemp, $scope.contractDates, $scope.contractData);
+			},function(error){
+
+			});
+		}
+
+		function addToDates(dateObj, date, item){
+			switch (date) {
+				case 'January':
+					dateObj.January.push(item);
+					return;
+				case 'February':
+					dateObj.February.push(item);
+					return;
+				case 'March':
+					dateObj.March.push(item);
+					return;
+				case 'April':
+					dateObj.April.push(item);
+					return;
+				case 'May':
+					dateObj.May.push(item);
+					return;
+				case 'June':
+					dateObj.June.push(item);
+					return;
+				case 'July':
+					dateObj.July.push(item);
+					return;
+				case 'August':
+					dateObj.August.push(item);
+					return;
+				case 'Sepetember':
+					dateObj.Sepetember.push(item);
+					return;
+				case 'October':
+					dateObj.October.push(item);
+					return;									
+				case 'November':
+					dateObj.November.push(item);
+					return;
+				case 'December':
+					dateObj.December.push(item);
+					return;
+				default:
+					return;
+			}
+		}
+
+		function getCountNumber(temp, dateObj, data){
+			for(var i = 0; i<$scope.labels.length; i++){
+				temp.push((dateObj[Object.keys(dateObj)[i]].length));
+			}
+			data.push(temp);
+		}
+
+		function getMonthName(item, description){
+			var date = moment(item[description]).format('MMMM');
+			return date;
+		}
+
+		// function countNumber(dateObj, items, dateName, temp, data){
+		// 	_.forEach(items, function(item){
+		// 		console.log(getMonthName(item, dateName));
+		// 		//addToDates(dateObj, date, item);	
+		// 	})
+		// 	//getCountNumber(temp, dateObj, data);
+		// }
+		
 		$scope.colours=[{
 			fillColor: 'rgba(255, 255, 255, 0.8)',
 		    strokeColor: 'rgba(255, 255, 255, 0.8)',
